dab5db0f24ece49719331a35f05c4d25c57c1424
Aman <amangiri381@gmail.com>
2023-11-30 01:49:58 +0530

    Firebase connection done and geting collage name form firebase

--- app/src/main/java/com/amzsoft/hostel/CollageNameActivity.java
+++ app/src/main/java/com/amzsoft/hostel/CollageNameActivity.java
@@ -1,5 +1,6 @@
 package com.amzsoft.hostel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,23 +11,29 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollageNameActivity extends AppCompatActivity {
+
+    private Spinner collegeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collage_name);
 
-        Spinner collegeSpinner = findViewById(R.id.collegeSpinner);
+        collegeSpinner = findViewById(R.id.collegeSpinner);
         Button nextButton = findViewById(R.id.nextButton);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.college_names,
-                android.R.layout.simple_spinner_item
-        );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        collegeSpinner.setAdapter(adapter);
+        // Fetch college names from Firestore and populate the spinner
+        fetchCollegeNames();
 
         collegeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -43,13 +50,46 @@ public class CollageNameActivity extends AppCompatActivity {
         });
     }
 
+    private void fetchCollegeNames() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("collage_name")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<String> collegeNames = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Assuming the document ID is the college name
+                                String collegeName = document.getId();
+                                collegeNames.add(collegeName);
+                            }
+
+                            // Populate the spinner with the retrieved college names
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                                    CollageNameActivity.this,
+                                    android.R.layout.simple_spinner_item,
+                                    collegeNames
+                            );
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            collegeSpinner.setAdapter(adapter);
+                        } else {
+                            // Handle errors
+                            Toast.makeText(
+                                    CollageNameActivity.this,
+                                    "Error fetching college names: " + task.getException(),
+                                    Toast.LENGTH_SHORT
+                            ).show();
+                        }
+                    }
+                });
+    }
+
     public void onNextButtonClick(View view) {
         // Handle the "Next" button click
-        Spinner collegeSpinner = findViewById(R.id.collegeSpinner);
         String selectedCollege = collegeSpinner.getSelectedItem().toString();
 
         // Perform actions with the selected college name (e.g., navigate to the next screen)
         Toast.makeText(this, "Selected College: " + selectedCollege, Toast.LENGTH_SHORT).show();
-
     }
 }
