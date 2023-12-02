5389bd468c24148041c9193a09bd5c304bde7b78
Aman <amangiri381@gmail.com>
2023-12-03 01:29:06 +0530

    side Navigation work completed

--- app/src/main/java/com/amzsoft/hostel/Admin/AdminMainActivity.java
+++ app/src/main/java/com/amzsoft/hostel/Admin/AdminMainActivity.java
@@ -1,5 +1,6 @@
 package com.amzsoft.hostel.Admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.amzsoft.hostel.Activity.CollageNameActivity;
 import com.amzsoft.hostel.R;
 import com.google.android.material.navigation.NavigationView;
 
@@ -26,41 +28,86 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
-        
-        drawerLayout =findViewById(R.id.adminDrawerLayout);
-        navigationView =findViewById(R.id.admimNavigationView);
-        toolbar =findViewById(R.id.admin_toolbar);
-        
+
+        drawerLayout = findViewById(R.id.adminDrawerLayout);
+        navigationView = findViewById(R.id.admimNavigationView);
+        toolbar = findViewById(R.id.admin_toolbar);
+
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(
-                this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
-        
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        // Disable the default drawer indicator
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(android.R.color.white));
+        toggle.setDrawerIndicatorEnabled(false);
 
-        loadFragment(new AdminMenuFragment());
+        // Set a custom icon for the toggle button
+        toggle.setHomeAsUpIndicator(R.drawable.baseline_menu_24);
 
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id =item.getItemId();
-                if(id == R.id.items1){
-                    loadFragment(new AdminContactFragment());
-                } else if (id ==R.id.items2) {
-                    loadFragment(new AdminMenuFragment());
-                }
-
+        toolbar.setNavigationOnClickListener(view -> {
+            // Handle the custom toggle button click here
+            if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
                 drawerLayout.closeDrawer(GravityCompat.START);
-
-                return true;
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
 
+        loadFragment(new AdminHomeFragment());
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle item clicks here
+                int id = item.getItemId();
+
+                if (id == R.id.admin_menu) {
+                    // Handle admin_menu click
+                    loadFragment(new AdminContactFragment());
+                } else if (id == R.id.admin_nearby_service) {
+                    // Handle admin_nearby_service click
+                    loadFragment(new AdminNearByFragment());
+                } else if (id == R.id.admin_Slider) {
+                    // Handle admin_Slider click
+                    loadFragment(new AdminSliderFragment());
+                } else if (id == R.id.admin_feedback) {
+                    // Handle admin_feedback click
+                    loadFragment(new AdminFeedbackFragment());
+                } else if (id == R.id.admin_contact) {
+                    // Handle admin_Contact click
+                    loadFragment(new AdminContactFragment());
+                } else if (id == R.id.admin_timing) {
+                    // Handle admin_timing click
+                    loadFragment(new AdminTimingFragment());
+                }else if (id == R.id.admin_support) {
+                    // Handle admin_Custmer_support click
+                    loadFragment(new CostmerSupportFragment());
+                }else if(id == R.id.admin_home){
+                    loadFragment(new AdminHomeFragment());
+                } else if (id == R.id.admin_logout) {
+                    Intent intent =new Intent(AdminMainActivity.this, CollageNameActivity.class);
+                    startActivity(intent);
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
+
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -72,10 +119,17 @@ public class AdminMainActivity extends AppCompatActivity {
     }
 
     private void loadFragment(Fragment fragment) {
-        FragmentManager fm =getSupportFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
 
-        ft.add(R.id.admin_container,fragment);
+        // Replace the existing fragment with the new one
+        ft.replace(R.id.admin_container, fragment);
+
+        // Add the transaction to the back stack
+        ft.addToBackStack(null);
+
+        // Commit the transaction
         ft.commit();
     }
+
 }
